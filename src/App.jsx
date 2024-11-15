cef12bcbbec5e4850571d3166fd6f9f026b61d89
abu-2003 <abubacker6855@gmail.com>
2024-11-15 15:07:14 +0530

    Fixed errors after upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import React from "react";
-import './app.css';
+import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
